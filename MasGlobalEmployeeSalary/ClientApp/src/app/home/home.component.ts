52afe54e282f9b77812dca23eae59d49add1af5d
julie <deisyjuliethgil@gmail.com>
2020-07-11 20:53:36 -0500

    Clean angular solution

--- MasGlobalEmployeeSalary/ClientApp/src/app/home/home.component.ts
+++ MasGlobalEmployeeSalary/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ApiService } from "./service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public employees: Employee[];
+  url;
+
+  constructor(private api: ApiService, @Inject('BASE_URL') baseUrl: string) {
+    this.url = baseUrl + 'api/employee';
+  }
+
+  ngOnInit() {
+    this.api
+      .getListOfGroup(this.url)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.employees = data;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
+}
+
+interface Employee {
+  Id: number,
+  Name: string,
+  ContractTypeName: string,
+  RoleId: number,
+  RoleName: string,
+  roleDescription: string,
+  hourlySalary: number,
+  monthlySalary: number
 }
