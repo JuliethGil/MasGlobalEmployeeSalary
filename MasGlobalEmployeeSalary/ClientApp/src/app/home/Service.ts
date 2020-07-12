6e89852747cfc7550d97cba966f7d4ba115a2f04
julie <deisyjuliethgil@gmail.com>
2020-07-12 12:56:38 -0500

    Calculate the salary depending on the type of contract

--- MasGlobalEmployeeSalary/ClientApp/src/app/home/Service.ts
+++ MasGlobalEmployeeSalary/ClientApp/src/app/home/Service.ts
@@ -16,36 +16,20 @@ const httpOptions = {
   providedIn: "root"
 })
 
-/**
- * Service to call all the API
- */
 export class ApiService {
   constructor(private http: HttpClient) { }
 
-  /**
-   * Function to handle error when the server return an error
-   *
-   * @param error
-   */
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.error("An error occurred:", error.error.message);
     } else {
-      // The backend returned an unsuccessful response code. The response body may contain clues as to what went wrong,
       console.error(
         'Backend returned code ${error.status}, ' + 'body was: ${error.error}'
       );
     }
-    // return an observable with a user-facing error message
     return throwError(error);
   }
 
-  /**
-   * Function to extract the data when the server return some
-   *
-   * @param res
-   */
   private extractData(res: Response) {
     let body = res;
     return body || {};
